6ff7f0ef722e19002f5ccad13fba1ea2455fb64a
Michael Mellinger <mmellinger66@gmail.com>
2016-03-09 09:29:19 -0500

    Added documentation

--- SimpleTableView/SimpleTableView/ViewController.swift
+++ SimpleTableView/SimpleTableView/ViewController.swift
@@ -10,9 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let tableViewController = SimpleTableViewController()
+    
+    func addTableView() {
+        
+        let viewDictionary:[String:AnyObject] = [
+            "tableView": tableViewController.tableView,
+            "topLayoutGuide": topLayoutGuide,
+
+        ]
+        tableViewController.tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(tableViewController.tableView)
+        
+        let metrics:[String:AnyObject] = [:]
+        
+        let h0Constraint = "H:|-[tableView]-|"
+        let v0Constraint = "V:[topLayoutGuide]-[tableView]-|"
+
+        [h0Constraint, v0Constraint].forEach {
+            
+            let constraint = NSLayoutConstraint.constraintsWithVisualFormat($0, options: [], metrics: metrics, views: viewDictionary)
+            
+            NSLayoutConstraint.activateConstraints(constraint)
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        addTableView()
     }
 
     override func didReceiveMemoryWarning() {
