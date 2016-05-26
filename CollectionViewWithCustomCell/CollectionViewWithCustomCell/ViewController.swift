ffa0044aa47e0f18d61942be95a5532e5bf6290b
Michael Mellinger <mmellinger66@gmail.com>
2016-05-26 13:11:49 -0400

    New project

--- CollectionViewWithCustomCell/CollectionViewWithCustomCell/ViewController.swift
+++ CollectionViewWithCustomCell/CollectionViewWithCustomCell/ViewController.swift
@@ -1,18 +1,56 @@
 //
 //  ViewController.swift
-//  CollectionViewWithCustomCell
+//  CollectionViewBasic
 //
-//  Created by Michael Mellinger on 5/26/16.
-//  Copyright © 2016 Michael Mellinger. All rights reserved.
+//  Created by Michael Mellinger on 5/24/16.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    var collectionViewController:BasicCollectionViewController!
+    
+    func addCollectionView() {
+        
+        let viewDictionary:[String:AnyObject] = [
+            "collectionView": collectionViewController.view,
+            "topLayoutGuide": topLayoutGuide,
+            
+            ]
+        collectionViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(collectionViewController.view)
+        
+        let metrics:[String:AnyObject] = [:]
+        
+        let h0Constraint = "H:|-[collectionView]-|"
+        let v0Constraint = "V:[topLayoutGuide]-[collectionView]-|"
+        
+        [h0Constraint, v0Constraint].forEach {
+            
+            let constraint = NSLayoutConstraint.constraintsWithVisualFormat($0, options: [], metrics: metrics, views: viewDictionary)
+            
+            NSLayoutConstraint.activateConstraints(constraint)
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let flowLayout=UICollectionViewFlowLayout()
+        flowLayout.itemSize = CGSizeMake(100, 100)
+        flowLayout.minimumInteritemSpacing = 2
+        flowLayout.minimumLineSpacing = 5
+        flowLayout.scrollDirection = .Vertical
+
+        // Must create with a FlowLayout if we create UICollectionViewController programatically
+        
+        collectionViewController = BasicCollectionViewController(collectionViewLayout: flowLayout)
+        
+        addCollectionView()
+
     }
 
     override func didReceiveMemoryWarning() {
