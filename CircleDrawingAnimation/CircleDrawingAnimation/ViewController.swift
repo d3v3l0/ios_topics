e3c7fac57d4006b9ee940b1a50031f64aa90a6ae
Michael Mellinger <mmellinger66@gmail.com>
2016-05-26 23:19:41 -0400

    cleanup

--- CircleDrawingAnimation/CircleDrawingAnimation/ViewController.swift
+++ CircleDrawingAnimation/CircleDrawingAnimation/ViewController.swift
@@ -10,9 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    func toRadians(degrees:CGFloat) -> CGFloat {
+        return degrees * CGFloat(M_PI)/180
+    }
+    
+    func addAnimatedCircleWithArc(){
+        
+        let layer = CAShapeLayer()
+        
+        let radius:CGFloat = 50
+        let center = CGPointMake(100, 350)
+        
+        let startAngle:CGFloat = toRadians(0)
+        let endAngle:CGFloat = toRadians(360)
+        
+        let clockwise = true
+        
+        layer.path = UIBezierPath(arcCenter: center,
+                                  radius: radius,
+                                  startAngle: startAngle,
+                                  endAngle: endAngle,
+                                  clockwise: clockwise).CGPath
+        
+        layer.fillColor = UIColor.clearColor().CGColor
+        layer.strokeColor = UIColor.redColor().CGColor
+        layer.lineWidth = 1
+        
+        view.layer.addSublayer(layer)
+        
+        let drawAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        drawAnimation.repeatCount = 1.0
+        
+        drawAnimation.fromValue = 0
+        drawAnimation.toValue = 1
+        
+        drawAnimation.duration = 5.0
+        
+        drawAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        
+        layer.addAnimation(drawAnimation, forKey: "drawCircleAnimation")
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        addAnimatedCircleWithArc()
+
     }
 
     override func didReceiveMemoryWarning() {
