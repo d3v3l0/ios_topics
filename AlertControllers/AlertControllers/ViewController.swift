955d2eb6fa524a88873bb6f8514705d6fd5172a3
Michael Mellinger <mmellinger66@gmail.com>
2016-06-01 18:07:10 -0400

    UIAlertController Examples

---
+++ AlertControllers/AlertControllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlertControllers
+//
+//  Created by Michael Mellinger on 6/1/16.
+//  Copyright © 2016 h4labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
