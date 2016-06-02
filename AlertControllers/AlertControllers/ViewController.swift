9d1274753589d9cd3776cb03b4d2dbda6241edd1
Michael Mellinger <mmellinger66@gmail.com>
2016-06-02 12:08:38 -0400

    UIAlertController Examples

--- AlertControllers/AlertControllers/ViewController.swift
+++ AlertControllers/AlertControllers/ViewController.swift
@@ -10,43 +10,57 @@ import UIKit
 class ViewController: UIViewController {
 
     let button1 = UIButton()
+    let label1 = UILabel()
+    
+    func addLabel() {
+        view.addSubview(label1)
+        label1.translatesAutoresizingMaskIntoConstraints = false
+        label1.text = "Click the Alert button"
+        label1.textColor = .grayColor()
+        label1.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor).active = true
+        label1.centerYAnchor.constraintEqualToAnchor(view.centerYAnchor, constant: 40).active = true
+
+    }
     
     func addButton() {
         view.addSubview(button1)
         button1.translatesAutoresizingMaskIntoConstraints = false
         button1.setTitle("Alert", forState: .Normal)
+        button1.addTarget(self, action: #selector(ViewController.showGameOverAlert(_:)), forControlEvents: .TouchUpInside)
         
         button1.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor).active = true
-        button1.centerYAnchor.constraintEqualToAnchor(view.centerYAnchor).active = true
+        button1.centerYAnchor.constraintEqualToAnchor(view.centerYAnchor, constant: -40).active = true
         
     }
     
-    @IBAction func showAlert(sender: AnyObject) {
-        // Initialize Alert Controller
-        let alertController = UIAlertController(title: "Alert", message: "Are you okay?", preferredStyle: .Alert)
+    func showGameOverAlert(button: UIButton) {
+
+        let alertController = UIAlertController(title: "Game Over", message: "Play again?", preferredStyle: .Alert)
         
-        // Initialize Actions
-        let yesAction = UIAlertAction(title: "Yes", style: .Default) { (action) -> Void in
-            print("The user is okay.")
+        // Using closure handler
+        let action1 = UIAlertAction(title: "Yes", style: .Default) { (action) -> Void in
+            self.label1.text = "Starting new game."
         }
         
-        let noAction = UIAlertAction(title: "No", style: .Default) { (action) -> Void in
-            print("The user is not okay.")
-        }
+        // Calling function handler
+        let action2 = UIAlertAction(title: "No", style: .Default, handler: noHandler(_:))
         
-        // Add Actions
-        alertController.addAction(yesAction)
-        alertController.addAction(noAction)
+        alertController.addAction(action1)
+        alertController.addAction(action2)
         
-        // Present Alert Controller
         self.presentViewController(alertController, animated: true, completion: nil)
     }
     
+    func noHandler(action: UIAlertAction) {
+        label1.text = "That's all folks!"
+
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = UIColor.purpleColor()
         addButton()
+        addLabel()
     }
 
     override func didReceiveMemoryWarning() {
