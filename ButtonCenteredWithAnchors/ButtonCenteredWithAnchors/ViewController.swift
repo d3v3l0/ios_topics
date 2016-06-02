39e758e21d12d6d5df44ebfc1b1760255c92aabe
Michael Mellinger <mmellinger66@gmail.com>
2016-06-02 13:07:14 -0400

    screenshots

---
+++ ButtonCenteredWithAnchors/ButtonCenteredWithAnchors/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  CenteredAutoLayoutButton
+//
+//  Created by Michael Mellinger on 8/14/14.
+//  Copyright (c) 2014 h4labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var centeredButton: UIButton!
+    
+    func addButton() {
+        centeredButton = UIButton()
+        centeredButton.setTitle("Am I centered?", forState: .Normal)
+        centeredButton.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        centeredButton.translatesAutoresizingMaskIntoConstraints = false
+        centeredButton.addTarget(self, action: #selector(ViewController.pressed(_:)), forControlEvents: .TouchUpInside)
+        
+        self.view.addSubview(centeredButton)
+        
+        centeredButton.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor).active = true
+        centeredButton.centerYAnchor.constraintEqualToAnchor(view.centerYAnchor, constant: -40).active = true
+    }
+    
+    func pressed(sender: UIButton!) {
+        
+        let title = "Awesome"
+        let message = "You did it"
+        let alert = UIAlertController(title: title,
+                                      message:message,
+                                      preferredStyle: .Alert)
+        let action = UIAlertAction(title: "Take Action 1?", style: .Default, handler:nil)
+        alert.addAction(action)
+        
+        let action2 = UIAlertAction(title: "Take Action 2?", style: .Default, handler:nil)
+        alert.addAction(action2)
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addButton()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
