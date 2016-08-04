dcd536b88ed4196f19412cdc29eddd9939d77230
Michael Mellinger <mmellinger66@gmail.com>
2016-08-04 11:51:58 -0400

    Swift 3

--- AlertControllers/AlertControllers/ViewController.swift
+++ AlertControllers/AlertControllers/ViewController.swift
@@ -16,49 +16,49 @@ class ViewController: UIViewController {
         view.addSubview(label1)
         label1.translatesAutoresizingMaskIntoConstraints = false
         label1.text = "Click the Alert button"
-        label1.textColor = .grayColor()
-        label1.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor).active = true
-        label1.centerYAnchor.constraintEqualToAnchor(view.centerYAnchor, constant: 40).active = true
+        label1.textColor = UIColor.gray
+        label1.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        label1.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 40).isActive = true
 
     }
     
     func addButton() {
         view.addSubview(button1)
         button1.translatesAutoresizingMaskIntoConstraints = false
-        button1.setTitle("Alert", forState: .Normal)
-        button1.addTarget(self, action: #selector(ViewController.showGameOverAlert(_:)), forControlEvents: .TouchUpInside)
+        button1.setTitle("Alert", for: UIControlState())
+        button1.addTarget(self, action: #selector(ViewController.showGameOverAlert(_:)), for: .touchUpInside)
         
-        button1.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor).active = true
-        button1.centerYAnchor.constraintEqualToAnchor(view.centerYAnchor, constant: -40).active = true
+        button1.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        button1.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -40).isActive = true
         
     }
     
-    func showGameOverAlert(button: UIButton) {
+    func showGameOverAlert(_ button: UIButton) {
 
-        let alertController = UIAlertController(title: "Game Over", message: "Play again?", preferredStyle: .Alert)
+        let alertController = UIAlertController(title: "Game Over", message: "Play again?", preferredStyle: .alert)
         
         // Using closure handler
-        let action1 = UIAlertAction(title: "Yes", style: .Default) { (action) -> Void in
+        let action1 = UIAlertAction(title: "Yes", style: .default) { (action) -> Void in
             self.label1.text = "Starting new game."
         }
         
         // Calling function handler
-        let action2 = UIAlertAction(title: "No", style: .Default, handler: noHandler(_:))
+        let action2 = UIAlertAction(title: "No", style: .default, handler: noHandler(_:))
         
         alertController.addAction(action1)
         alertController.addAction(action2)
         
-        self.presentViewController(alertController, animated: true, completion: nil)
+        self.present(alertController, animated: true, completion: nil)
     }
     
-    func noHandler(action: UIAlertAction) {
+    func noHandler(_ action: UIAlertAction) {
         label1.text = "That's all folks!"
 
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor.purpleColor()
+        view.backgroundColor = UIColor.purple
         addButton()
         addLabel()
     }
