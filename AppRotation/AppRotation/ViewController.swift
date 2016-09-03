5af7e5f4a395d9ee401423a5b2939570fff730b1
Michael Mellinger <mmellinger66@gmail.com>
2016-09-03 10:42:05 -0400

    App that handles device rotation

---
+++ AppRotation/AppRotation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppRotation
+//
+//  Created by Michael Mellinger on 9/3/16.
+//  Copyright © 2016 h4labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
