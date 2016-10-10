0195d3bfdae82617798177d9350411d6acef6bc4
Michael Mellinger <mmellinger66@gmail.com>
2016-10-09 23:55:12 -0400

    Countdown timer app

---
+++ CountDownTimer/CountDownTimer/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  CountDownTimer
+//
+//  Created by Michael Mellinger on 10/9/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let timerLabel = UILabel()
+    var secondsRemaining = 5
+    var isTimerRunning = true
+    var timer:Timer!
+    
+    
+    func startTimer() {
+        timer = Timer()
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    func updateTimer() {
+        
+        secondsRemaining -= 1
+        
+        if secondsRemaining == 0 {
+            isTimerRunning = false
+            timer.invalidate()
+            timerLabel.text = "Done"
+        } else {
+            timerLabel.text = "\(secondsRemaining)"
+        }
+    }
+    
+    func buildView() {
+        
+        timerLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(timerLabel)
+        
+        timerLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        timerLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        //timerLabel.layer.borderWidth = 2
+        //timerLabel.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        timerLabel.font = UIFont.systemFont(ofSize: 36)
+        timerLabel.text = "\(secondsRemaining)"
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .purple
+        buildView()
+        
+        startTimer()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
