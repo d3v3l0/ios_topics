295076486175f182d8ac15627250c46a1d190b66
Michael Mellinger <mmellinger66@gmail.com>
2016-12-16 09:42:49 -0500

    Programmatically show a ViewController

---
+++ ShowViewController/ShowViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShowViewController
+//
+//  Created by Michael Mellinger on 12/16/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .purple
+    }
+
+}
+
