74d4ff1577019d78b2648b61b5eb8b11e94db0cf
Michael Mellinger <mmellinger66@gmail.com>
2016-12-16 18:00:34 -0500

    View Transforms

---
+++ ViewTransforms/ViewTransforms/ViewController.swift
@@ -0,0 +1,116 @@
+//
+//  ViewController.swift
+//  ViewTransforms
+//
+//  Created by Michael Mellinger on 12/16/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let label = UILabel()
+    let aBtn  = UIButton()
+    let aView = UIView()
+    
+    func buildView() {
+        
+        aView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(aView)
+        aView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        aView.backgroundColor = .brown
+        aView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -100).isActive = true
+        aView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        aView.widthAnchor.constraint(greaterThanOrEqualToConstant: 50).isActive = true
+        //
+        label.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(label)
+        label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        
+        label.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        
+        //        label.backgroundColor = .white
+        //        label.contentEdgeInsets = UIEdgeInsetsMake(5, 5, 5, 5)
+        
+        label.text = "foo"
+        
+        //
+        aBtn.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(aBtn)
+        aBtn.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        aBtn.setTitle("Transform", for: .normal)
+        aBtn.layer.borderWidth = 1
+        
+        aBtn.contentEdgeInsets = UIEdgeInsetsMake(5, 5, 5, 5)
+        
+        aBtn.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 100).isActive = true
+        aBtn.addTarget(self, action: #selector(scaleIt(sender:)), for: .touchUpInside)
+    }
+    
+    func scaleIt(sender:UIButton) {
+        //
+        UIView.animate(withDuration: 1,
+                       delay: 0,
+                       options: .curveEaseInOut,
+                       animations: {
+                        
+                        var transform = CGAffineTransform.identity
+                        transform = transform.scaledBy(x: 4, y: 4)
+                        self.label.transform = transform
+                        self.aView.transform = CGAffineTransform(scaleX: 6, y: 6)
+        }, completion: {_ in
+            self.rotateIt()
+        })
+    }
+    
+    func rotateIt() {
+        //
+        UIView.animate(withDuration: 2,
+                       delay: 0,
+                       options: .curveEaseInOut,
+                       animations: {
+                        
+                        let transform = CGAffineTransform.identity.rotated(by: .pi)
+                        
+                        self.label.transform = CGAffineTransform(rotationAngle: .pi)
+                        
+                        self.aView.transform = transform
+        }, completion: {_ in
+            
+            self.fadeIt()
+        })
+    }
+    
+    
+    func fadeIt() {
+        
+        UIView.animate(withDuration: 2,
+                       delay: 0,
+                       options: .curveEaseInOut,
+                       animations: {
+                        
+                        self.aView.alpha = 0
+                        
+        }, completion: {_ in
+            
+            UIView.animate(withDuration: 2,
+                           delay: 0,
+                           options: .curveEaseInOut,
+                           animations: {
+                            
+                            self.aView.alpha = 1
+            }, completion: {_ in
+                
+            })
+        })
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .purple
+        buildView()
+        
+    }
+    
+}
+
