76403bc36e06389dee2c6cbab4c8bd83dc43103e
Michael Mellinger <mmellinger66@gmail.com>
2017-02-13 09:39:02 -0500

    UISwitch

---
+++ SwitchController/SwitchController/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  SwitchController
+//
+//  Created by Michael Mellinger on 2/12/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let aSwitch = UISwitch()
+    let label = UILabel()
+    
+    func buildView() {
+        aSwitch.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(aSwitch)
+        
+        
+        aSwitch.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        aSwitch.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        aSwitch.widthAnchor.constraint(equalToConstant: 150)
+        
+        //
+        let margins = self.view.layoutMarginsGuide
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(label)
+        label.topAnchor.constraint(equalTo: margins.topAnchor, constant: 100).isActive = true
+        label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .lightGray
+        buildView()
+        
+        aSwitch.tintColor = .blue
+        aSwitch.isOn = true
+        label.text = "\(aSwitch.isOn)"
+        aSwitch.addTarget(self, action: #selector(switchValueDidChange(_:)), for: .valueChanged)
+        
+    }
+    
+    func switchValueDidChange(_ sender:UISwitch) {
+        let value = aSwitch.isOn
+        label.text = "\(value)"
+    }
+
+
+}
+
