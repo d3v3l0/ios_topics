ee58257f6e3ac2909a6c4b2ded1fc65fc084d8ba
Michael Mellinger <mmellinger66@gmail.com>
2020-04-30 15:27:52 -0400

    Label Attributes SwiftUI

---
+++ LabelAttributes/LabelAttributes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LabelAttributes
+//
+//  Created by Michael Mellinger on 4/30/20.
+//  Copyright © 2020 h4labs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
